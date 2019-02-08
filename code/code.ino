85f47e41767ad334cb8aeda1ce6826c74864031d
petterlovereide <petter.lovereide@gmail.com>
2019-02-08 13:52:08 +0100

    Added blinking lights for wifi and firebase

--- code/code.ino
+++ code/code.ino
@@ -8,7 +8,7 @@
 #define FIREBASE_AUTH "ykor3oRgZ8UYirKJf827ZHZkXnqEa8xwxiMvNv0j"
 #define WIFI_SSID "HACKERMAN"
 #define WIFI_PASSWORD "RaspBerryPi19"
-#define LED 2
+
 #define BUILDING "Voksenåsen Hotell"
 #define SENSORID "100"
 
@@ -25,48 +25,46 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 const int MotionSensorSignal = 5;  // Digital port for the motion sensor
-
 int motionSensorValue = 0;
 
 float tempSensorValue = 0.0;
 
-int DA=A0;  // Pin for Analog Output - AO
-int threshold = 100; //Change this
+const int LightSensorSignal = A0;  // Pin for Analog Output - AO
 float lightSensorValue = 0.0;
 
+// Led Lights
+int ledRedPin = 4;
+int ledGreenPin = 2;
 
 ///////////////////////////////////////////
 
 void sensorLoop() {
-
- //MOTION
-
+//MOTION
  motionSensorValue = digitalRead(MotionSensorSignal);
-
  Serial.print("This is the value from the motion sensor: ");
  Serial.println(motionSensorValue);
 
-
 //TEMP
-
  // call sensors.requestTemperatures() to issue a global temperature
  // request to all devices on the bus
- Serial.print(" Requesting temperatures...");
  sensors.requestTemperatures(); // Send the command to get temperatures
- Serial.println("DONE");
 
  Serial.print("Temperature is: ");
  tempSensorValue = sensors.getTempCByIndex(0);
- Serial.println(tempSensorValue); // Why "byIndex"?
-   // You can have more than one IC on the same bus.
-   // 0 refers to the first IC on the wire
+ Serial.println(tempSensorValue);
+ // You can have more than one IC on the same bus.
+ // 0 refers to the first IC on the wire
 
-//LUX
-
- lightSensorValue = analogRead(DA);  //Read the analog value
+//LIGHT
+ lightSensorValue = analogRead(LightSensorSignal);  //Read the analog value
  Serial.print("Light is: ");
  Serial.println(lightSensorValue);  //Print the analog value
+}
 
+void blinkLED(int pin){
+  digitalWrite(pin, HIGH);
+  delay(500);
+  digitalWrite(pin, LOW);
 }
 
 void connectToWifi() {
@@ -74,7 +72,7 @@ void connectToWifi() {
  Serial.print("Connecting to WIFI");
  while (WiFi.status() != WL_CONNECTED) {
    Serial.print(".");
-   delay(500);
+   blinkLED(ledRedPin);
  }
  Serial.println();
  Serial.print("Connected with IP: ");
@@ -86,16 +84,23 @@ void connectToFirebase() {
  if (Firebase.failed()){
    Serial.print("Firebase init failed: ");
    Serial.println(Firebase.error());
+   digitalWrite(ledRedPin, HIGH);
  }
- if (Firebase.success()){
+ else if (Firebase.success()){
    Serial.println("Firebase init success!");
  }
+}
+
+void setupLEDs() {
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
 }
 
 ///////////////////////////////////////////
 
 void setup() {
  Serial.begin(9600);
+ setupLEDs();
  connectToWifi();
  connectToFirebase();
 
@@ -104,30 +109,12 @@ void setup() {
 }
 
 void loop() {
-
- /*
- Firebase.setInt("myValue", 123);
- int myValue = Firebase.getInt("myValue");
- Serial.print("myValue: ");
- Serial.println(myValue);
- */
-
- // Print error if something failed
- if (Firebase.failed()) {
-   Serial.print("Something failed: ");
-   Serial.println(Firebase.error());
- }
-
  sensorLoop();
 
  if(tempSensorValue < -50) {
    return;
-   }
-/*
- Firebase.setFloat("temp", tempSensorValue);
- Firebase.setBool("motion", motionSensorValue);
- Firebase.setFloat("lux", lightSensorValue);
-*/
+ }
+
  DynamicJsonBuffer jsonBuffer;
 
  // Push to Firebase
@@ -139,6 +126,14 @@ void loop() {
  tempTime[".sv"] = "timestamp";
 
  Firebase.push("sensors/300", temperatureObject);
+ if(Firebase.success()){
+  Serial.println("Firebase push success!");
+  blinkLED(ledGreenPin);
+ }
+ else if(Firebase.failed()){
+  Serial.println("Firebase push failed!");
+  blinkLED(ledRedPin);
+ }
 
- delay(10000);
+ delay(9500);
 }
